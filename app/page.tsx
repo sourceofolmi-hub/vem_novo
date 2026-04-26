55a57f5278fb8076b8d300be21ec02dc924bdd14
Vem_T_Aki <sourceofolmi@gmail.com>
2026-04-26 14:03:01 +0100

    Fix site updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import CartButton from "./components/CartButton";
 
 export default function Page() {
